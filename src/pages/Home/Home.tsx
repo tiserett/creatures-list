732def2fa7cb7afec53dfbe4b13085b5f3237d8b
Maks Sobko <tiserettt@gmail.com>
2022-11-09 17:41:21 +0200

    home page description was added

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,46 @@
 import React from 'react';
 
 export const Home = () => (
-  <h1>Home Page</h1>
+  <section className="hero is-medium is-primary">
+    <div className="hero-body">
+      <p className="title is-1 has-text-black-bis mb-6">
+        Welcome to People table
+        <br />
+        Done by Maksym Sobko
+      </p>
+      <article className="
+        subtitle
+        is-size-3
+        has-text-black-bis
+        has-text-weight-bold"
+      >
+        <p>
+          On top of each page you
+          can see header with navigation.
+        </p>
+        <p>
+          On the People page you can see table
+          with information about certain people.
+        </p>
+        <p>
+          Each row contains information about person&apos;s:
+          id, name, username, email, city and street.
+        </p>
+        <p>
+          In the end of each row you can see:
+          open, edit and delete buttons.
+        </p>
+        <p>
+          Below table you can see Add new person button,
+          which opens modal window with form.
+        </p>
+        <p>
+          This project is deployed via github pages.
+          <br />
+          Styles were written using bulma.
+        </p>
+      </article>
+    </div>
+  </section>
+
 );
